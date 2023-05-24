2925483c3e349c29f7909df825d562a2356eb75b
Степан Крылов <kiriki00@mail.ru>
2023-05-24 12:41:11 +0700

    server action

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось! И у сервера тоже!!!')
+    return HttpResponse('У меня получилось! И у сервера тоже!!')
 
 
 def second_page(request):
